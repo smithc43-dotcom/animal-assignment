090e0cbb84e7dd8b49a64af1500391f446dcc5bf
Conner Smith <smithc43@oregonstate.edu>
2026-01-05 19:54:30 -0700

    Add animal.py assignment

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Conner Smith
+# GitHub username: smithc43
+# Date: 1/5/2026
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
