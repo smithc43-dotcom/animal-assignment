3e16e0188239e7cc534bccbab6760da5450bc9ff
Conner Smith <smithc43@oregonstate.edu>
2026-01-08 15:02:26 -0700

    Add average program

---
+++ average.py
@@ -0,0 +1,17 @@
+# Author: Conner Smith
+# GitHub username: smithc43
+# Date: 1/5/2026
+# Description: Asks the user for five numbers and prints the average
+
+print("Please enter five numbers.")
+
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+
+average = (num1 + num2 + num3 + num4 + num5) / 5
+
+print("The average of those numbers is:")
+print(average)
