b15032109144800ee80a7d73a0b8170e5122a763
Conner Smith <smithc43@oregonstate.edu>
2026-01-08 15:09:34 -0700

    Add temp conversion program

---
+++ temp_convert.py
@@ -0,0 +1,10 @@
+# Author: Conner Smith
+# GitHub username: smithc43
+# Date: 1/8/2026
+# Description: Converts a Celsius temperature to Fahrenheit
+
+celsius_temp = float(input("Please enter a Celsius temperature.\n"))
+fahrenheit_temp = (9 / 5) * celsius_temp + 32
+
+print("The equivalent Fahrenheit temperature is:")
+print(fahrenheit_temp)
